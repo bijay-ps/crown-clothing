da8abc6538a6db41f2f2ec05324b8e0cfc1b50d5
BIJAY SINGH <bijay.prakash7@yahoo.in>
2019-12-04 22:31:31 +0530

    added homepage component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const HomePage = () => (
     <div className="homepage">
@@ -9,6 +10,36 @@ const HomePage = () => (
             <span className="subtitle">SHOP NOW</span>
           </div>
         </div>
+  
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+  
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SNEAKERS</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+  
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">WOMEN</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+  
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">MEN</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
       </div>
     </div>
 );
+
+export default HomePage;
