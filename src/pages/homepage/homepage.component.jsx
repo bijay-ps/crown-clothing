5654ab2b4f0a975018c68b1a53a654a9f5dd6b53
bijay_ps <bijay.prakash7@yahoo.in>
2019-12-09 12:27:51 +0530

    added routing

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './homepage.styles.scss';
 import DirectoryComponent from "../../components/directory/directory.component";
 
-const HomePage = () => (
+const HomePage = ({ history }) => (
     <div className="homepage">
       <DirectoryComponent />
     </div>
