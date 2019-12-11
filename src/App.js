f48b43fa8aba4809c94c539f45c28a1ce2556aac
bijay_ps <bijay.prakash7@yahoo.in>
2019-12-11 14:19:48 +0530

    added 'Add to Cart' functionality

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ export class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
+          <Route path="/shop" component={ShopPage} />
           <Route
             exact
             path="/signIn"
