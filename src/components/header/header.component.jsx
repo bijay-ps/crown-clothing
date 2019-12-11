716facce6c6285a4eddbe4ce49c7ba860adfdb05
bijay_ps <bijay.prakash7@yahoo.in>
2019-12-11 11:53:32 +0530

    added Redux initial configuration to project in Header component.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from "../../firebase/firebase.utils";
 
 import './header.styles.scss';
+
 
 const Header = ({ currentUser }) => (
     <div className='header'>
@@ -32,4 +35,9 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
-export default Header;
+// state is root reducer
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
